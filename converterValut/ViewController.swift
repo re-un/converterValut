6988b399ce8a08e0dd79922df3f82d07da7b527b
Андрей Лютынский <coca-cola@macbook-pro-andrew.local>
2018-08-17 11:34:30 +0300

    Initial Commit

---
+++ converterValut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  converterValut
+//
+//  Created by Andrew Lutinskyi on 17.08.2018.
+//  Copyright © 2018 Andrew Lutinskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
