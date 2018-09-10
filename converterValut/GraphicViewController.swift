a3d2e3f1c5903084b7cfc1c44bbda9f7d21c753c
re-un <androlutik@gmail.com>
2018-09-10 22:44:22 +0300

    tap on graph to see currency on specific day

--- converterValut/GraphicViewController.swift
+++ converterValut/GraphicViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class GraphicViewController: UIViewController {
     var curAbbreviation = ""
     var dlabels = [UILabel]()
-    var months = ["","","","","",""]
+    var separator = UIView()
+    var currentXSeparatorPosition:CGFloat = 0
     var dataGraph = [dataAlgorithm.graphCurrence](){
         didSet{
             DispatchQueue.main.async {
@@ -20,6 +21,39 @@ class GraphicViewController: UIViewController {
             for index in dataGraph.indices{
                 grView.graphicData += [dataGraph[index].Cur_OfficialRate]
             }
+        }
+    }
+    @IBAction func tapOnGraph(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .began,.changed,.ended:
+            var point = sender.location(in: grView)
+            point.y = 10
+            separator.frame = CGRect(origin: point, size: separator.frame.size)
+            separator.isHidden = false
+            currentXSeparatorPosition = separator.frame.origin.x
+            var elementOfArray = Int(currentXSeparatorPosition/grView.bounds.maxX*CGFloat(dataGraph.count))
+            elementOfArray = dataGraph.count - elementOfArray - 1
+            print("current \(currentXSeparatorPosition), grviewmaxX \(grView.bounds.maxX)")
+            print(elementOfArray)
+            curAbbreviationLabel.text = String(dataGraph[elementOfArray].Cur_OfficialRate) + " " + data.dateToString(date: dataGraph[elementOfArray].Date, dateFormat: "dd MMM")
+        default:
+            break
+        }
+    }
+    @IBAction func tapOnScreen(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            var point = sender.location(in: grView)
+            point.y = 10
+            separator.frame = CGRect(origin: point, size: separator.frame.size)
+            separator.isHidden = false
+            currentXSeparatorPosition = separator.frame.origin.x
+            let elementOfArray = Int(currentXSeparatorPosition/grView.bounds.maxX * CGFloat(dataGraph.count - 1))
+            print("current \(currentXSeparatorPosition)")
+            print(elementOfArray)
+            curAbbreviationLabel.text = String(dataGraph[elementOfArray].Cur_OfficialRate) + " " + data.dateToString(date: dataGraph[elementOfArray].Date, dateFormat: "dd MMM")
+        default:
+            break
         }
     }
     
@@ -33,16 +67,16 @@ class GraphicViewController: UIViewController {
     @IBOutlet weak var dateLabels: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        separator = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: view.frame.maxY))
+        separator.backgroundColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
+        grView.addSubview(separator)
+        separator.isHidden = true
         spinner.startAnimating()
         // Do any additional setup after loading the view.
         DispatchQueue.global(qos: .userInitiated).async {
             [weak self] in
             self?.dataGraph = data.getDataForGraph(curAbbreviation: (self?.curAbbreviation)!)
         }
-        
-//        for index in data.dataForGraphic.indices{
-//            grView.graphicData += [data.dataForGraphic[index].Cur_OfficialRate]
-//        }
         dlabels = (dateLabels.arrangedSubviews as? [UILabel])!
         for index in dlabels.indices{
             let date = Calendar.current.date(byAdding: .month, value: index*2, to: Date())
